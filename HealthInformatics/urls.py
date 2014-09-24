edbf7dd64600ff43785c38dcfcf94ce1e171930d
Neal Williams <nealfwilliams@gmail.com>
2014-09-24 07:06:34 -0700

    hello world

--- HealthInformatics/urls.py
+++ HealthInformatics/urls.py
@@ -6,12 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'healthapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^thank-you/', 'healthapp.views.thankyou', name='thank you'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^mission/', 'healthapp.views.mission', name='mission'),
     )
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- healthapp/views.py
+++ healthapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render_to_response, RequestContext, HttpResponseRedirect
 from django.contrib import messages
 
+def home(request):
+    return render_to_response("thankyou.html",
+                              locals(),
+                              context_instance=RequestContext(request))
