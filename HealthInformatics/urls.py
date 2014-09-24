fc3c97492ef14d2de8a45f50a65985489ecd1932
Neal Williams <nealfwilliams@gmail.com>
2014-09-24 05:22:18 -0700

    urls changed

--- HealthInformatics/urls.py
+++ HealthInformatics/urls.py
@@ -15,4 +15,3 @@ urlpatterns = patterns('',
     )
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
