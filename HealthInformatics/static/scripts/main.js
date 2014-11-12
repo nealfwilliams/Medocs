bf0860241dea8c793170aad653d41528ce8fce68
Neal Williams <nealfwilliams@gmail.com>
2014-11-12 15:42:43 -0800

    polished css / timeline text

--- HealthInformatics/static/scripts/main.js
+++ HealthInformatics/static/scripts/main.js
@@ -1,24 +1,21 @@
 var nav_icon = $("#nav_toggle");
 
-var timeline_options = {
-    width: '100%',
-    height: '300px',
-};
-
-var timeline_groups = {
-
-};
 var menu_toggle = function(){
     var out = false;
+
     if (nav_icon.hasClass("glyphicon-chevron-right")){
         out = true;
     }
-    var animation1 = "+=20%";
-    var animation2 = "-=20%";
-    if (out){
+
+    var animation1 = "+=15%";
+    var animation2 = "-=15%";
+
+    if (out)
+    {
         nav_icon.toggleClass("glyphicon-chevron-left");
         nav_icon.toggleClass("glyphicon-chevron-right")
     }
+
     else{
         temp = animation1;
         animation1 = animation2;
@@ -26,21 +23,21 @@ var menu_toggle = function(){
         nav_icon.toggleClass("glyphicon-chevron-left");
         nav_icon.toggleClass("glyphicon-chevron-right")
     }
+
     $(".left_nav").animate({
         left: animation1
     }, 1000);
+
     $("#content").animate({
         width: animation2
     }, 1000);
 };
 
 var create_timeline = function(){
-
 };
 
 /*var change_items = function(item_list){
     for (item in item_list) {
-
     }
 };*/
 
@@ -52,6 +49,37 @@ nav_icon.click(function(){
 $("#menu_in").click(function(){
     menu_toggle(False, out_icon, in_icon);
 });
+
+
+$(".nav_content").click(function() {
+    console.log("function called");
+    $(this).children(".check_items").show();
+    var notClicked = $('.nav_content').not(this);
+    notClicked.each(function() {
+        $(this).children(".check_items").hide();
+    });
+});
+
+var container = document.getElementById('visualization');
+var options = {
+  width: '100%',
+  height: '200px',
+  margin: {
+    item: 20
+  }
+};
+
+var test_items = new vis.DataSet([
+    {id: 1, content: 'item 1', start: '2013-04-20'},
+    {id: 2, content: 'item 2', start: '2013-04-14'},
+    {id: 3, content: 'item 3', start: '2013-04-18'},
+    {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
+    {id: 5, content: 'item 5', start: '2013-04-25'},
+    {id: 6, content: 'item 6', start: '2013-04-27'}
+  ]);
+
+var timeline = new vis.Timeline(container, test_items, options);
+
 
 /*$("#timeline_nav").click({
 
